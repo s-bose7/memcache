970df709284c43a7f2fb51a1e1f09ca5866a2a62
Sudip Bose <sbose007ime.work@gmail.com>
2024-02-22 13:40:35 +0530

    MapItem: Each keys will point to this MapItem, contains Map values and other metadata

--- include/MapItem.h
+++ include/MapItem.h
@@ -1,16 +1,19 @@
 #ifndef MAPITEM_H
 #define MAPITEM_H
 
-#include "NodeList.h"
+#include "KeyNode.h"
 #include "FrequencyNode.h"
 
 class MapItem {
 public:
+    /* int value: holds the value for a particular key */
     int value;
+    /* FrequencyNode* parent: pointer to the parent frequency node */
     FrequencyNode *parent;
-    NodeList *node;
-
-    MapItem(int value, FrequencyNode *parent, NodeList *node);
+    /* KeyNode *node: pointer to the node that holds the key under parent */
+    KeyNode *node;
+    /* constructor */
+    MapItem(int value, FrequencyNode *parent, KeyNode *node);
 };
 
 #endif
