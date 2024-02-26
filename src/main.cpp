f002b83f359cd3d907f75fb8e3c86db8bdcd3dfc
Sudip Bose <sbose007ime.work@gmail.com>
2024-02-26 23:01:15 +0530

    main program

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,19 @@ using namespace std;
 int main() {
     // Create an LFUCache object with capacity 3
     LFUCache cache(3);
-
-    // Test get and put operations
     cache.put(1, 1);
     cache.put(2, 2);
     cache.put(3, 3);
-    cout << "Value for key 1: " << cache.get(1) << endl; // Expected: 1
-    cout << "Value for key 2: " << cache.get(2) << endl; // Expected: 2
-    cout << "Value for key 4: " << cache.get(4) << endl; // Expected: -1
-    cache.put(4, 4); // Evicts key 3
-    cout << "Value for key 3: " << cache.get(3) << endl; // Expected: -1 (evicted)
-    cout << "Value for key 4: " << cache.get(4) << endl; // Expected: 4
-
+    cache.put(4, 4);
+    cout<<cache.get(4)<<endl;
+    cout<<cache.get(3)<<endl;
+    cout<<cache.get(2)<<endl;
+    cout<<cache.get(1)<<endl;
+    cache.put(5, 5);
+    cout<<cache.get(1)<<endl;
+    cout<<cache.get(2)<<endl;
+    cout<<cache.get(3)<<endl;
+    cout<<cache.get(4)<<endl;
+    cout<<cache.get(5)<<endl;
     return 0;
 }
