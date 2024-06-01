c4baf11adfc919e098b68996e44999fb24225212
Sudip Bose <sbose007ime.work@gmail.com>
2024-06-01 13:10:34 +0530

    Added new test to verify the core-logic

--- tests/test_memcache.cpp
+++ tests/test_memcache.cpp
@@ -5,22 +5,39 @@
 // Define test fixtures if needed
 class MemCacheTest : public ::testing::Test {
 protected:
-    // You can define common setup routines here
+    MemCache* cache;
+    int DEFAULT_SIZE = 2;
+
+    // Define common setup routines here
     virtual void SetUp() {
         // Initialize common objects, etc.
+        cache = new MemCache(DEFAULT_SIZE);
     }
 
-    // You can define common tear-down routines here
+    // Define common tear-down routines here
     virtual void TearDown() {
         // Clean up common resources, etc.
+        delete cache;
     }
     
 };
 
-TEST_F(MemCacheTest, InsertionAndRetrieval){
-    MemCache cache{10};
-    cache.put(1, 10);
-    EXPECT_EQ(cache.get(1), 10);
+// If this test fails, that means recent changes affect the core logic.
+TEST_F(MemCacheTest, CheckLogicalAccuracy){
+    cache->put(1, 1);
+    cache->put(2, 2);
+    EXPECT_EQ(cache->get(1), 1);
+    cache->put(3, 3);
+    EXPECT_EQ(cache->get(2), -1);
+    EXPECT_EQ(cache->get(3), 3);
+    cache->put(4, 4);
+    EXPECT_EQ(cache->get(1), -1);
+    EXPECT_EQ(cache->get(3), 3);
+    EXPECT_EQ(cache->get(4), 4);
+}
+
+TEST_F(MemCacheTest, VerifyOperationRemove){
+    EXPECT_EQ(cache->remove(4), true);
 }
 
 int main(int argc, char **argv) {
