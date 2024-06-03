696c1adf7a9e3e1c188f64bdc7f7bae64555b20d
Sudip Bose <sbose007ime.work@gmail.com>
2024-06-03 13:27:29 +0530

    Test for clear method

--- tests/test_memcache.cpp
+++ tests/test_memcache.cpp
@@ -12,7 +12,6 @@ protected:
     // Define common setup routines here
     virtual void SetUp() {
         // Initialize common objects, etc.
-        cache = new MemCache(2);
     }
 
     // Define common tear-down routines here
@@ -25,6 +24,7 @@ protected:
 
 TEST_F(MemCacheTest, VerifyTTLFunction){
     int ttl = 2;
+    cache = new MemCache(1);
     cache->put(22, 2147483640, ttl);
     this_thread::sleep_for(chrono::seconds(ttl*3));
     EXPECT_EQ(cache->get(22), -1);
@@ -33,6 +33,7 @@ TEST_F(MemCacheTest, VerifyTTLFunction){
 
 // If this test fails, that means recent changes affect the core logic.
 TEST_F(MemCacheTest, CheckLogicalAccuracy){
+    cache = new MemCache(2);
     cache->put(1, 1);
     cache->put(2, 2);
     EXPECT_EQ(cache->get(1), 1);
@@ -46,6 +47,7 @@ TEST_F(MemCacheTest, CheckLogicalAccuracy){
 }
 
 TEST_F(MemCacheTest, VerifyOperationRemove){
+    cache = new MemCache(1);
     cache->put(4, 4);
     EXPECT_EQ(cache->remove(4), true);
     EXPECT_EQ(cache->exists(4), false);
@@ -56,6 +58,23 @@ TEST_F(MemCacheTest, VerifyOperationRemove){
     EXPECT_EQ(cache->remove(2), false);
 }
 
+TEST_F(MemCacheTest, VeryfyClearOperation){
+    cache = new MemCache(10);
+    for(int k=0; k<10; k++) {
+        int val = k+2;
+        if(k>0 && k%2 == 0){
+            val = cache->get(k-1);
+        }
+        cache->put(k, val);
+    }
+    EXPECT_EQ(cache->get(2), 3);
+    EXPECT_EQ(cache->get(6), 7);
+    EXPECT_EQ(cache->get(8), 9);
+    cache->clear();
+    EXPECT_EQ(cache->get(2), -1);
+    EXPECT_EQ(cache->get(6), -1);
+    EXPECT_EQ(cache->get(8), -1);
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
